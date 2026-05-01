d88dfe36168a7023c77c135836248bad976cb5eb
sparescaffolding <mylovelycar.2010@gmail.com>
2026-05-01 18:08:10 +0400

    added slope handling to theplayer controller

--- Assets/Scripts/Player/Player_Controller.cs
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -2,23 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public enum Player_MovementState
 {
     walk,
     run,
+    crouch,
     air
 }
 
 public class Player_Controller : MonoBehaviour
 {
     public Player_MovementState movement_state;
+    //move and jump speeds/forces
     private float currentspeed;                         //the speed which gets applied
     public float walkspeed;
     public float runspeed;
     public float jumpspeed;
     public float airmultiplier;
     public float drag;
+    //crouching
+    [Space]
+    public float crouchscale;
+    private float normalscale;
+    
+    
     public Transform orientation;
     
     //check stuff (grounded? etc.)
@@ -28,6 +37,7 @@ public class Player_Controller : MonoBehaviour
     //bools
     private bool grounded;
     private bool canjump = true;
+    private bool exitingslope;
     
     
     //input stuff
@@ -37,11 +47,15 @@ public class Player_Controller : MonoBehaviour
     private Vector3 movedirection;                      //the direction the player is moving towards
     private Rigidbody rigidbody;
 
+    private RaycastHit hit_slope;
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         //freeze all rotations
         rigidbody.freezeRotation = true;
+        //set the player height
+        normalscale = transform.localScale.y;
     }
 
     private void Update() {
@@ -75,11 +89,27 @@ public class Player_Controller : MonoBehaviour
         //jump functionality
         if (Input.GetKey(KeyCode.Space) && canjump && grounded)
         {
-            //reset jump
+            //reset jump and start exiting slope
             canjump = false;
+            exitingslope = true;
             PlayerJump();
             //delay the next jump by a quarter of a second
             Invoke(nameof(PlayerJumpReset), 0.25f);
+        }
+        
+        //start player crouching
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            //set the player Y scale to crouch scale
+            transform.localScale = new Vector3(transform.localScale.x, crouchscale, transform.localScale.z);
+            //push player down towards ground, so that way theres no space below player when initializing crouch
+            rigidbody.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+        //end player crouching
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            //set the player Y scale back to original scale
+            transform.localScale = new Vector3(transform.localScale.x, normalscale, transform.localScale.z);
         }
     }
 
@@ -101,6 +131,13 @@ public class Player_Controller : MonoBehaviour
         {
             movement_state = Player_MovementState.air;
         }
+        
+        //crouching
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            movement_state = Player_MovementState.crouch;
+            currentspeed = walkspeed / 2;
+        }
     }
     
     private void PlayerMove()
@@ -118,17 +155,45 @@ public class Player_Controller : MonoBehaviour
         {
             rigidbody.AddForce(movedirection.normalized * currentspeed * 10 * airmultiplier, ForceMode.Force);
         }
+        
+        //if the player is on a slope angle (angle amount in accordance to max_slope in PlayerSlope()
+        if (PlayerSlope())
+        {
+            rigidbody.AddForce(PlayerCalculateSlopeDirection() * currentspeed * 20f, ForceMode.Force);
+            //if player going upwards
+            if (rigidbody.velocity.y > 0)
+            {
+                //apply a downward force
+                rigidbody.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+        }
+        
+        //disable gravity if on a slope
+        rigidbody.useGravity = !PlayerSlope();
     }
 
     private void PlayerSpeedControl()
     {
-        //limit velocity when going faster than movespeed
-        Vector3 flat_velocity = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-        if (flat_velocity.magnitude > currentspeed) {
-            //calculate limited speed
-            Vector3 limited_velocity = flat_velocity.normalized * currentspeed;
-            //apply
-            rigidbody.velocity = new Vector3(limited_velocity.x, rigidbody.velocity.y, limited_velocity.z);
+        //control speed when on slope
+        if (PlayerSlope() && !exitingslope)
+        {
+            if (rigidbody.velocity.magnitude > currentspeed)
+            {
+                //mimic regular currentspeed when going up or down a slope
+                rigidbody.velocity = rigidbody.velocity.normalized * currentspeed;
+            }
+        }
+        //control the players speed on ground or air
+        else
+        {
+            //limit velocity when going faster than movespeed
+            Vector3 flat_velocity = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
+            if (flat_velocity.magnitude > currentspeed) {
+                //calculate limited speed
+                Vector3 limited_velocity = flat_velocity.normalized * currentspeed;
+                //apply
+                rigidbody.velocity = new Vector3(limited_velocity.x, rigidbody.velocity.y, limited_velocity.z);
+            }
         }
     }
 
@@ -143,5 +208,24 @@ public class Player_Controller : MonoBehaviour
     private void PlayerJumpReset()
     {
         canjump = true;
+        exitingslope = false;
+    }
+
+    private bool PlayerSlope()
+    {
+        //max angle at which slope handling would be handled at
+        float max_slope = 40;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit_slope, playerheight * 0.5f + 0.2f))
+        {
+            float angle = Vector3.Angle(Vector3.up, hit_slope.normal);
+            return angle < max_slope;
+        }
+
+        return false;
+    }
+
+    private Vector3 PlayerCalculateSlopeDirection()
+    {
+        return Vector3.ProjectOnPlane(movedirection, hit_slope.normal).normalized;
     }
 }
