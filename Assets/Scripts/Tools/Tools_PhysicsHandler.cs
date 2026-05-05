abbe9d9e717bef1f2c50cdf346ec73e69b4c6686
sparescaffolding <mylovelycar.2010@gmail.com>
2026-05-05 12:00:18 +0400

    physhandler animations

--- Assets/Scripts/Tools/Tools_PhysicsHandler.cs
+++ Assets/Scripts/Tools/Tools_PhysicsHandler.cs
@@ -13,11 +13,29 @@ public class Tools_PhysicsHandler : MonoBehaviour
     public bool is_being_used = false;
     private Player_Interactor player_interactor;
     private RaycastHit hit;
+    private Animator animator;
 
     private void Start()
     {
         //find player interactor
         player_interactor = FindFirstObjectByType<Player_Interactor>();
+        //animator init
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        //if holding via tool
+        if (player_interactor.pickup.holding && player_interactor.pickup.using_tool)
+        {
+            //start moving claw things (start animation)
+            animator.SetBool("holding", true);
+        }
+        else
+        {
+            //exit animation if not holding
+            animator.SetBool("holding", false);
+        }
     }
 
     private void OnEnable()
